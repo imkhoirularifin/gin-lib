8c31ecebce1ab9055573bc4de28715d139652dc5
Khoirul Arifin <khoirularfin12@gmail.com>
2025-04-18 16:45:47 +0700

    docs: add docs

--- gin_lib.go
+++ gin_lib.go
@@ -2,6 +2,8 @@ package ginlib
 
 import "fmt"
 
+// GinLib prints the version information of the GinLib library to the console.
+// This function is intended to provide a simple way to verify the library version.
 func GinLib() {
 	fmt.Println("GinLib Version 1.0.0")
 }
